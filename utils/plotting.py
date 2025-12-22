1c5ad3f0092c9eac9961d9890db5cc240e9af3bd
BoydVdP <Boydvanderplaat@gmail.com>
2025-12-22 19:25:28 +0100

    Delete utils directory

--- utils/plotting.py
+++
@@ -1,6 +0,0 @@
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from amuse.plot import *
-
-plt.style.use('https://github.com/dhaitz/matplotlib-stylesheets/raw/master/pitayasmoothie-dark.mplstyle')
